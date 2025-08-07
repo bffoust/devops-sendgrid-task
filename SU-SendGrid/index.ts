a8fd818f447677241cfefcbac6100cc89da4a2f9
Bruce Foust <bfoust@stampinup.com>
2025-08-07 15:29:17 -0600

    more input

--- SU-SendGrid/index.ts
+++ SU-SendGrid/index.ts
@@ -2,16 +2,41 @@ import tl = require('azure-pipelines-task-lib/task');
 
 async function run() {
   try {
-    const inputString: string | undefined = tl.getInput('samplestring', true);
-    if (inputString == 'bad') {
+    const inputString1: string | undefined = tl.getInput('SendGridAPIKey', true);
+    if (inputString1 == 'bad') {
       tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
       return;
     }
-    console.log('Hello', inputString);
+    console.log('APIKey:', inputString1);
   }
   catch (err: any) {
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
+
+  try {
+    const inputString2: string | undefined = tl.getInput('senderEmailAddress', true);
+    if (inputString2 == 'bad') {
+      tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+      return;
+    }
+    console.log('Send mail from:', inputString2);
+  }
+  catch (err: any) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
+
+  try {
+    const inputString3: string | undefined = tl.getInput('recipientEmailAddress', true);
+    if (inputString3 == 'bad') {
+      tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+      return;
+    }
+    console.log('Send mail to:', inputString3);
+  }
+  catch (err: any) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
+
 }
 
 run();
